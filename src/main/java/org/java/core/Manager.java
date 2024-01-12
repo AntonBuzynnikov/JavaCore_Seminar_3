8de37ae948d93937057ff600a83e12e07f767631
Anton <buzynnikov2111@gmail.com>
2024-01-12 17:59:38 +0500

    Update Manager

--- src/main/java/org/java/core/Manager.java
+++ src/main/java/org/java/core/Manager.java
@@ -9,10 +9,5 @@ public class Manager extends Employee{
     public static void increaseSalary(List<Employee> employees, int amount,int age){
         List<Employee> temp =
                 employees.stream().filter(e-> e.getAge()>age).filter(x->!(x instanceof Manager)).peek(x->x.setSalary(x.getSalary()+amount)).toList();
-//        for (Employee item:temp){
-//            if(!(item instanceof Manager)){
-//                item.setSalary(item.getSalary() + amount);
-//            }
-//        }
     }
 }
